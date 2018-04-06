55a3056c580fcf5808ae31629f753d88e14d80fc
f0s3 <power.aerozol@gmail.com>
2018-04-06 13:05:37 +0300

    #7 Edited task1.cpp for lab4.

--- semester2/2/programming/lab4/task1.cpp
+++ semester2/2/programming/lab4/task1.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <cmath>
-
-
-using namespace std;
+#include <iomanip>
 
 int main() {
-    double x,y;
     int counter = 0;
-    for (x = -0.50;x <= 0;x += 0.01) {
-        y = ((2 * x + 1) / pow(x - 1, 2));
+    std::string space = "";
+    std::cout << ".-------------------------------.\n";
+    std::cout << "|______x________" << "|______y________|\n";
+    for (double x = -0.50;x <= 0;x += 0.01) {
+        double y = ((2 * x + 1) / pow(x - 1, 2));
         counter++;
-        cout << "y" << counter << " = " << y << endl;
+        std::cout << std::fixed << std::setprecision(2);
+        (counter <= 9) ? space = " " : space = "";
+        std::cout << "|  x" << counter << space <<" =  " << x << " |  y" << counter << "  =  " << y << space << " |\n";
     }
-
-
+    std::cout << "`-------------------------------`\n";
     return 0;
 }
