d82cb8a4edfe346f96965e2b37a6966b49e7fd7f
f0s3 <power.aerozoll@gmail.com>
2018-04-13 10:54:55 +0300

    Completed(hopefully) task1 for lab3 of second variant.

--- semester2/2/programming/lab3/task1.cpp
+++ semester2/2/programming/lab3/task1.cpp
@@ -3,11 +3,31 @@
 using namespace std;
 
 int main() {
-    int value, result = 1;
-    cout << "Enter a value: ";
+    int value, result = 1, userChoice;
+    cout << "Enter value: ";
     cin >> value;
-    if ((value & 1) == 0) value--;
-    for (int i = value; i >= 1; i -= 2) result = result * i;
-    cout << result << endl;
+	cout << "1. while" << endl;
+	cout << "2. do-while" << endl;
+	cin << userChoice;
+	switch (userChoice) {
+		case 1:
+			if ((value & 1) == 0) value--;
+			int i = value;
+			while (i >= 1) {
+				result = result * i;
+				i -= 2;
+			}
+			cout << result << endl;
+			break;
+		case 2:
+			if ((value & 1) == 0) value--;
+			int i = value;
+			do {
+				result = result * i;
+				i -= 2;
+			} while (i >= 1);
+			cout << result << endl;
+			break;
+	}
     return 0;
 }
