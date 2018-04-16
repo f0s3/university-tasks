e9006c36fc293952925db121dac9e514d3713016
f0s3 <power.aerozoll@gmail.com>
2018-04-16 09:45:42 +0300

    #10 Added comp_logic folder.

--- semester2/2/programming/lab3/task2.cpp
+++ semester2/2/programming/lab3/task2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     double i = 1;
-    while (i <= 1.007) {
+    while (s <= 0.001) {
         double s = 1 / pow(2,i);
         cout << "s" << i << " = " << s << endl;
         i += 0.001;
