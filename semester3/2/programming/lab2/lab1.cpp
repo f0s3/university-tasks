be374d59911e3fcfbc00632e25ff874a1633f8ae
f0s3 <power.aerozoll@gmail.com>
2019-05-30 17:22:46 +0300

    Refactor code

--- semester3/2/programming/lab2/lab1.cpp
+++ semester3/2/programming/lab2/lab1.cpp
@@ -5,29 +5,15 @@
 using namespace std;
 
 int main() {
-    double x = 6.;
-    double f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) - 7*x + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = -6.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = 3.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = -3.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = 2.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = -2.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = 1.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
-    x = -1.;
-    f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) + 6;
-    if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
+    double []xValues = {6., -6., 3., -3., 2., -2., 1., -1.};
+    double x = 0;
+    double f = 0;
+
+    for (int i = 0;i < xValues.length;i++) {
+        x = xValues[i];
+        f = 6*pow(x, 4) + 7*pow(x, 3) - 36*pow(x, 2) - 7*x + 6;
+        if (f == 0.) printf("f(x) =%6.2f x =%f\n", f, x);
+    }
+
     return 0;
 }
